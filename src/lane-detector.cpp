e174a733ca527bb019ea3547fee016be69744220
Christian Berger <christian.berger@gu.se>
2018-11-13 22:55:28 +0100

    * binary thresholding
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- src/lane-detector.cpp
+++ src/lane-detector.cpp
@@ -36,10 +36,11 @@ int32_t main(int32_t argc, char **argv) {
          (0 == commandlineArguments.count("height")) ) {
         std::cerr << argv[0] << " attaches to a shared memory area containing an ARGB image." << std::endl;
         std::cerr << "Usage:   " << argv[0] << " --cid=<OD4 session> --name=<name of shared memory area> [--verbose]" << std::endl;
-        std::cerr << "         --cid:    CID of the OD4Session to send and receive messages" << std::endl;
-        std::cerr << "         --name:   name of the shared memory area to attach" << std::endl;
-        std::cerr << "         --width:  width of the frame" << std::endl;
-        std::cerr << "         --height: height of the frame" << std::endl;
+        std::cerr << "         --cid:       CID of the OD4Session to send and receive messages" << std::endl;
+        std::cerr << "         --name:      name of the shared memory area to attach" << std::endl;
+        std::cerr << "         --width:     width of the frame" << std::endl;
+        std::cerr << "         --height:    height of the frame" << std::endl;
+        std::cerr << "         --threshold: binary threshold value (default: 115)" << std::endl;
         std::cerr << "Example: " << argv[0] << " --cid=253 --name=img.argb --width=640 --height=480 --verbose" << std::endl;
     }
     else {
@@ -47,6 +48,10 @@ int32_t main(int32_t argc, char **argv) {
         const uint32_t WIDTH{static_cast<uint32_t>(std::stoi(commandlineArguments["width"]))};
         const uint32_t HEIGHT{static_cast<uint32_t>(std::stoi(commandlineArguments["height"]))};
         const bool VERBOSE{commandlineArguments.count("verbose") != 0};
+
+        ////////////////////////////////////////////////////////////////////////
+        // Parameters for lane-detector.
+        const uint32_t THRESHOLD{commandlineArguments.count("verbose") != 0 ? static_cast<uint32_t>(std::stoi(commandlineArguments["threshold"])) : 115};
 
         // Attach to the shared memory.
         std::unique_ptr<cluon::SharedMemory> sharedMemory{new cluon::SharedMemory{NAME}};
@@ -75,6 +80,7 @@ int32_t main(int32_t argc, char **argv) {
             // Endless loop; end the program by pressing Ctrl-C.
             while (od4.isRunning()) {
                 cv::Mat img;
+                cv::Mat originalImage;
 
                 // Wait for a notification of a new frame.
                 sharedMemory->wait();
@@ -95,6 +101,12 @@ int32_t main(int32_t argc, char **argv) {
                 if (VERBOSE) {
                     cv::imshow(sharedMemory->name().c_str(), img);
                     cv::waitKey(1);
+                }
+
+                ////////////////////////////////////////////////////////////////
+                {
+                    // Copy image.
+                    img.copyTo(originalImage);
                 }
 
                 ////////////////////////////////////////////////////////////////
@@ -126,6 +138,22 @@ int32_t main(int32_t argc, char **argv) {
                         cv::waitKey(1);
                     }
                 }
+
+                ////////////////////////////////////////////////////////////////
+                {
+                    // Apply binary threshold.
+                    cv::threshold(img, img, THRESHOLD, 255, CV_THRESH_BINARY);
+
+                    if (VERBOSE) {
+                        std::stringstream sstr;
+                        sstr << sharedMemory->name() << "-threshold";
+                        const std::string windowName = sstr.str();
+                        cv::imshow(windowName.c_str(), img);
+                        cv::waitKey(1);
+                    }
+                }
+
+
 
                 ////////////////////////////////////////////////////////////////
                 // Example for creating and sending a message to other microservices; can
